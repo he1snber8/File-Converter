ab7d6fa390ed8915c948fdac5ad9abace791b9b5
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-21 10:09:21 +0400

    Add App.vue

---
+++ client/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container py-4">
+    <h1 class="mb-4">File Converter</h1>
+    <FileConverter />
+  </div>
+</template>
+
+<script setup>
+import FileConverter from './components/FileConverter.vue';
+</script>
+
+<style scoped>
+</style>
