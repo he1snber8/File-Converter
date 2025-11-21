284b0de17f009447507c6f3e541db34377354aa3
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-21 10:09:21 +0400

    Add graphqlClient.js

---
+++ client/src/graphqlClient.js
@@ -0,0 +1,12 @@
+const endpoint = 'http://localhost:4000/graphql';
+
+export async function gqlRequest(query, variables) {
+  const res = await fetch(endpoint, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query, variables }),
+  });
+  const json = await res.json();
+  if (json.errors) throw new Error(json.errors[0].message);
+  return json.data;
+}
